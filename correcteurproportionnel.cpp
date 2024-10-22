f3c79ee7a32e0da89da3c4326ec3e5b1ff805f46
BELMELIANI ELIAS <belm0015@028F2-0F0002010.local.iut-troyes.univ-reims.fr>
2024-10-22 09:53:15 +0200

    Classe mère Correcteur et classe fille CorrecteurProportionnel

---
+++ correcteurproportionnel.cpp
@@ -0,0 +1,18 @@
+#include "correcteurproportionnel.h"
+
+
+CorrecteurProportionnel::CorrecteurProportionnel(double Kp, double consigne)
+    : Kp(Kp), consigne(consigne)
+{
+
+}
+
+// recoit valeur via comportement en entree
+double CorrecteurProportionnel::process(double entree)
+{
+
+    double erreur = consigne - entree;
+    double commande = Kp * erreur;
+    return commande;
+
+}
