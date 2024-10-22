0bd378353644b8859d5cb4e57c676c959fa0afcf
STEVENIN TRISTAN <stev0009@028F2-0F0002005.local.iut-troyes.univ-reims.fr>
2024-10-22 10:12:49 +0200

    Ajout de la connection au serveur

--- tcpclient.cpp
+++ tcpclient.cpp
@@ -1,11 +1,15 @@
 #include "tcpclient.h"
 #include <QDebug>
 
-TcpClient::TcpClient(): dataIn(&clientSocket)
+ TcpClient::TcpClient(QString _AddrIp, int _port) :clientSocket(this)
 
 {
-    dataIn.setDevice(&clientSocket);
+    AddrIp=_AddrIp;
+    port=_port;
+    dataIn.setDevice(&clientSocket) ;
+    connect(&clientSocket, &QTcpSocket::readyRead, this, &TcpClient::receiveDatas);
 }
+
 
 void TcpClient::sendDatas(QString datas)
 {
@@ -17,3 +21,23 @@ void TcpClient::sendDatas(QString datas)
     //serveurSocket->write(block);
 }
 
+void TcpClient::connectToServer()
+{
+    clientSocket.connectToHost(AddrIp,port);
+    if (!clientSocket.waitForConnected(3000)) { // Attendre 3 secondes
+        qDebug() << "impossible de se connecter au serveur";
+        return;
+    }
+    qDebug() << "Connecté au serveur sur" << AddrIp << ":" << port;
+}
+
+
+void TcpClient::receiveDatas()
+{
+    dataIn.startTransaction();
+    if (!dataIn.commitTransaction()) return;
+    QString receivedData;
+    dataIn >> receivedData;
+    emit newDatas(receivedData);
+    qDebug() << "Message reçu :" << receivedData;
+}
--- tcpclient.h
+++ tcpclient.h
@@ -1,22 +1,41 @@
 #ifndef TCPCLIENT_H
 #define TCPCLIENT_H
-
+#include <QObject>
 #include <QTcpSocket>
 #include <QTcpServer>
 #include <QDataStream>
 
-class TcpClient
+/*
+    Pour utiliser La classe :
+    IP serveur de simulation : 10.98.32.15
+    Port disponible : 8880 - 8885
+    Creé un objet TcpClient, lui donner l'ip, puis le serveur
+    Appeler la methode connectToServer pour se connecter
+
+ */
+
+
+class TcpClient: public QObject
 {
+Q_OBJECT
+
 public:
-    TcpClient();
-    void sendDatas(QString datas);
+     TcpClient(QString _AddrIp, int _port);
+     void sendDatas(QString datas);
+     void connectToServer();
+
+public slots:
+    void receiveDatas();
+
+
+signals:
+    void newDatas(QString datas);
 
 private :
     QTcpSocket clientSocket;
     QDataStream dataIn;
-
-signals:
-    void newDatas(QString datas);
+    QString AddrIp;
+    int port;
 
 };
 
