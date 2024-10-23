082c6f97e82a353371f3a4ae24faeadcd4280962
mezd-yasmina <mezdyasmina@gmail.com>
2024-10-23 14:17:26 +0200

    Update suivrebord.cpp

--- suivrebord.cpp
+++ suivrebord.cpp
@@ -11,7 +11,7 @@ SuivreBord::SuivreBord(array<int,360>&distanceLidar, Correcteur &c1, int _angle,
 void SuivreBord::process()
 {
 
-     double distanceBord = tabdistance.at(angle+180);  // Lire la distance à gauche (-180° dans tabdistance[359])"pour suivre le mur gauche"
+     double distanceBord = distanceLidar.at(angle+180);  // Lire la distance à gauche (-180° dans tabdistance[359])"pour suivre le mur gauche"
     qDebug() << "la distance au bord" <<distanceBord;
     double direction = c.process(distanceBord);
     qDebug() << "l'angle de correction est de "<<direction;
