febd372712fc83f535e7af8d062af02334b3b1da
mezd-yasmina <mezdyasmina@gmail.com>
2024-10-24 10:36:47 +0200

    Update tcpclient.h

--- tcpclient.h
+++ tcpclient.h
@@ -1,28 +1,57 @@
-#ifndef TCPSERVEUR_H
-#define TCPSERVEUR_H
-#include "qstring.h"
+#ifndef TCPCLIENT_H
+#define TCPCLIENT_H
+#include <QObject>
 #include <QTcpSocket>
 #include <QTcpServer>
 #include <QDataStream>
-#include "QObject"
+#include <QTimer>
 
-class TcpServeur: public QObject
+/*
+    !! Il faut ajouter QT += network dans le .pro !!
+
+Pour utiliser la classe :
+
+    IP du serveur de simulation : 10.98.32.154
+    Ports disponibles : 8880 - 8885
+    Créer un objet TcpClient (par exemple crée l'objet dans le mainwindow.h et lui donner ses parametres dans le mainwindow.cpp),
+    lui assigner l'IP, puis le port du serveur. L'IP doit etre entre guillemets. Par exemple : tcp1("10.98.32.154", 8885)
+
+    Une fois l'objet créé, il se connectera automatiquement au serveur.
+    Chaque donnée reçue déclenchera le signal newData, qui contiendra les données
+
+*/
+
+
+class TcpClient: public QObject
 {
     Q_OBJECT
+
 public:
-    TcpServeur();
-    QTcpSocket  *serveurSocket=nullptr;
-    void sendDatas(QString _datas);
-    QDataStream dataln;
-    QTcpServer monServeur;
-private :
+    TcpClient(QString _AddrIp, int _port);
+    void sendDatas(QString datas);
+    void connectToServer();
+    void connecte();
+    void connexion();
 
-public slots :
+public slots:
+    void receiveDatas();
+    void reconnect();
 
-    void recoitDatas();
-    void onNewConnection(/*QTcpSocket  *serveurSocket*/) ;
-signals :
+signals:
     void newDatas(QString datas);
+
+private :
+    QTcpSocket clientSocket;
+
+    QDataStream dataIn;
+    QDataStream dataOut;
+
+    QString AddrIp;
+
+    int port;
+
+    QTimer reconnectTimer;
+
 };
 
-#endif // TCPSERVEUR_H
+#endif // TCPCLIENT_H
