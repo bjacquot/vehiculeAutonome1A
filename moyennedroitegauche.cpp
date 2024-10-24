9bb267c6ea28c62af66eb75805dee4d78f273ca3
eliasbelme <elias.belmeliani3@gmail.com>
2024-10-24 18:01:46 +0200

    Update moyennedroitegauche.cpp
    changement avec bouton

--- moyennedroitegauche.cpp
+++ moyennedroitegauche.cpp
@@ -1,43 +1,63 @@
 #include "moyennedroitegauche.h"
 #include "QDebug"
-
-
 using namespace std;
 
-MoyenneDroiteGauche::MoyenneDroiteGauche(array<int,360>&distanceLidar, Correcteur &c1, int _angle, double _vitesse)
-    : Comportement(distanceLidar), c{c1},angle(_angle), vitesse (_vitesse)
+MoyenneDroiteGauche::MoyenneDroiteGauche(array<int,360>&distanceLidar, Correcteur &c1,int _angle1, int _angle2, double _vitesse )
+    : Comportement(distanceLidar), c{c1}, vitesse(_vitesse)
 {
-
+    angle1 = (-1)*_angle1;
+    angle2 =  _angle2;
 }
-
 
 void MoyenneDroiteGauche::process()
 {
-    double distanceGauche= distanceLidar.at(angle+180);
-    double distanceDroite= distanceLidar.at(-angle+180);
-    double distanceDevant= distanceLidar.at(180);
+   if  (etatVoiture==true)
+      {
 
-    double entree=distanceGauche-distanceDroite;
+    //distance gauche
+    double distanceGauche= distanceLidar.at(angle1+180);
+    //distance droite
+    double distanceDroite= distanceLidar.at(angle2+180);
+    //difference entre les 2
+    double entree=(distanceGauche- distanceDroite);
+
 
 
     //envoit des valeurs vers Correcteur
     double direction=c.process(entree);
 
+
     if (direction>1)
-    {direction=1;}
+    {
+        direction=1;
+    }
     else if (direction<-1)
-    {direction=-1;}
+    {
+        direction=-1;
+    }
+    else
+    {
+        direction=direction;
+    }
 
-    if (vitesse>1)
-    {vitesse=1;}
-    else if (vitesse<-1)
-    {vitesse=-1;}
+    emit deplacer(vitesse ,direction);
+        }
 
 
-    qDebug()<< distanceGauche <<distanceDroite;
+   else
+   {
+      emit deplacer(0 ,0);
+  }
 
-    //direction = -1;
-    //vitesse = 0.5;
-    emit deplacer(vitesse,direction);
-    //envoit a Materiel
 }
+
+void MoyenneDroiteGauche::controlerEtat(QString message)
+{
+  if (message == "on") etatVoiture==true;
+  else if (message == "off") etatVoiture==false;
+}
+
+
+
+
+
