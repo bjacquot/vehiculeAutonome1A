5e892cd67b0caa67cf4c5f07c3cb61bb6349cb35
MICHEL ADRIEN <mich0218@028F2-0F0002016.local.iut-troyes.univ-reims.fr>
2024-10-24 17:08:13 +0200

    Derniere mise a jour (redefinition de la direction int --> double)

--- moyennedroitegauche.cpp
+++ moyennedroitegauche.cpp
@@ -17,44 +17,22 @@ void MoyenneDroiteGauche::process()
     double distanceDroite= distanceLidar.at(-angle+180);
     double distanceDevant= distanceLidar.at(180);
 
-/*
-    double angleMin=60;
-    double angleMax = 120;
-
-    for(int i=angleMin;i<angleMax;i++){
-        if (distanceGauche< distanceLidar.at(i+180)){
-        distanceGauche= distanceLidar.at(i+180);
-        }
-        if (distanceDroite< distanceLidar.at(-i+180)){
-        distanceDroite= distanceLidar.at(-i+180);
-        }
-        //recupere la valeur la plus proche de chaque coté, entre angleMin et angleMax
-    }*/
-
-
     double entree=distanceGauche-distanceDroite;
 
 
     //envoit des valeurs vers Correcteur
-    int direction=c.process(entree);
-
-    qDebug()<< distanceGauche <<distanceDroite <<entree << direction;
+    double direction=c.process(entree);
 
     if (direction>1)
-    {
-        direction=1;
-    }
+    {direction=1;}
     else if (direction<-1)
-    {
-        direction=-1;
-    }
-    else
-    {
-        direction=direction;
-    }
+    {direction=-1;}
 
-    vitesse=distanceDevant/1000;
-    //fait changer la vitesse du robot en fonction de la distance du mur de devant
+    if (vitesse>1)
+    {vitesse=1;}
+    else if (vitesse<-1)
+    {vitesse=-1;}
+
 
     qDebug()<< distanceGauche <<distanceDroite;
 
